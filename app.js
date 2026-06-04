f1786dacd2bd0eb4228627f4a56601e5b9b1e652
Dianna Song <damolda.trade@gmail.com>
2026-06-04 17:09:42 +0900

    logo

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function layout(title='DAMOLDA'){
   document.title = title;
   const nav = `
   <header class="top">
-    <a class="brand" href="${href(\'index.html\')}"><img class="brand-handwordmark" src="assets/damolda-handwordmark.png" alt="다몰다"></a>
+    <a class="brand" href="${href('index.html')}"><img class="brand-handwordmark" src="assets/damolda-handwordmark.png" alt="다몰다"></a>
     <nav class="nav"><a href="${href('themes.html')}">${t[lang].themes}</a><a href="${href('journal.html')}">${t[lang].journal}</a><a href="${href('about.html')}">${t[lang].about}</a></nav>
     <div class="right-tools"><input id="search" class="search" placeholder="${t[lang].search}" autocomplete="off"><div class="lang"><a class="${lang==='ko'?'active':''}" href="?lang=ko">한국어</a> | <a class="${lang==='en'?'active':''}" href="?lang=en">EN</a></div></div>
   </header><div id="searchPanel" class="search-panel"></div>`;
